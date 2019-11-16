1b663e5759f40ef0dfd3bb45886882b880fb6689
reidwil <44632147+reidwil@users.noreply.github.com>
2019-11-15 20:52:31 -0600

    Add files via upload

---
+++ app.R
@@ -0,0 +1,23 @@
+# App
+
+# Libraries
+library(prophet)
+library(dplyr)
+library(tibble)
+
+# Sources
+source("C:\Users\Reid\Documents\R\prophet\R\main\utils.R")
+
+main <- function(plot = FALSE, data = FALSE) {
+  
+  if(!plot && !data || plot && data) {print("Warning - Input one desired outcome: Plot or Data")}
+  
+  if(!plot && data ) {return(run_forecast_with(x = formatted_data(), data.plot = 'Data'))}
+  
+  if(plot && !data ) {return(run_forecast_with(x = formatted_data(), data.plot = 'Plot'))}
+
+  
+}
+
+
+main(F, F)
