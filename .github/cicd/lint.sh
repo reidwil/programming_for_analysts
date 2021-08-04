127d83fdae65b306d4e71373624145b737ae1e2e
reidwil <44632147+reidwil@users.noreply.github.com>
2021-08-04 11:58:47 -0500

    Update lint.sh

--- .github/cicd/lint.sh
+++ .github/cicd/lint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This is necessary for formatting github messages
 echo '```'
+git branch --show-current
 diff-quality --violations sqlfluff --compare-branch=remotes/origin/develop
 echo '```'
