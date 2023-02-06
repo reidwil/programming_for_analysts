01a7450f84309d6138002facd1225020b3b71a51
reidwil <44632147+reidwil@users.noreply.github.com>
2023-02-06 13:51:57 -0500

    Update db.py

--- duck_app/db.py
+++ duck_app/db.py
@@ -4,10 +4,12 @@ import pandas
 class DB:
     def __init__(self):
         self.con = duckdb.connect()
+
     @staticmethod
     def df_to_html(df: pandas.DataFrame):
         return df.to_html(index=False)
 
-    def query_file(self, file) -> pandas.DataFrame:
-        results = self.con.execute(f"select * from '{file}'").df()
+    def query_file(self, file, query) -> pandas.DataFrame:
+        query = query.replace(":file", f"'{file}'")
+        results = self.con.execute(query).df()
         return self.df_to_html(results)
